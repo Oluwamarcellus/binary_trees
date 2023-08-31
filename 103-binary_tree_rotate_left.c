d4d5cfc53abb0ebe80c5ae8c25d42bc2fc4382cf
Noransaber <noransaber685@gmail.com>
2023-08-31 09:33:22 +0300

    Updates

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 binary_tree_t *p;
 
-if (tree == NULL || tree-> right == NULL)
+if (tree == NULL || tree->right == NULL)
 {
 return (NULL);
 }
